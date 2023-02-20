ddcee48f92214ed931e5381ef69c3c411cfaf7a6
beyarkay <boydrkane@gmail.com>
2023-02-20 13:37:29 +0200

    Add better calendar description

--- create-calendars.py
+++ create-calendars.py
@@ -8,6 +8,7 @@ def main():
     daily = []
     hourly = []
 
+    print("Getting weather data")
     res = requests.get("http://wttr.in/Cape%20Town?format=j1")
     if res.ok:
         try:
@@ -36,10 +37,30 @@ def main():
                     end = item["date"] + "T" + f"{hour:0>2}:{minute:0>2}:00"
                     temp = hr["tempC"]
                     pressure = hr["pressure"]
+                    desc: str = WWO_CODE[hr["weatherCode"]]
+                    emoji = WEATHER_SYMBOL.get(desc, "")
+                    desc = (
+                        "".join([(c if c.islower() else " " + c.lower()) for c in desc])
+                        .strip()
+                        .capitalize()
+                    )
+                    lines = [
+                        f"Feels Like: {hr['FeelsLikeC']}",
+                        f"Wind Speed: {hr['windspeedKmph']}km/h",
+                        f"Wind Direction: {hr['winddir16Point']}",
+                        f"Wind Gust: {hr['WindGustKmph']}km/h",
+                        f"Cloud Cover: {hr['cloudcover']}%",
+                        f"Humidity: {hr['humidity']}%",
+                        f"Precipitation: {hr['precipMM']}mm",
+                        f"Pressure: {hr['pressure']}hPa",
+                        f"visibility: {hr['visibility']}km",
+                        f"Chance of Rain: {hr['chanceofrain']}%",
+                        f"Chance of Sunshine: {hr['chanceofsunshine']}%",
+                    ]
                     hourly.append(
                         {
-                            "title": f"{temp}°C, {pressure}hPa",
-                            "description": f"no description",
+                            "title": f"{emoji} {desc} {temp}°C {pressure}hPa",
+                            "description": "\n".join(lines),
                             "start": start,
                             "end": end,
                         }
@@ -67,6 +88,80 @@ def main():
         yaml.dump({"events": hourly}, file)
 
     print(f"Python script finished")
+
+
+WWO_CODE = {
+    "113": "Sunny",
+    "116": "PartlyCloudy",
+    "119": "Cloudy",
+    "122": "VeryCloudy",
+    "143": "Fog",
+    "176": "LightShowers",
+    "179": "LightSleetShowers",
+    "182": "LightSleet",
+    "185": "LightSleet",
+    "200": "ThunderyShowers",
+    "227": "LightSnow",
+    "230": "HeavySnow",
+    "248": "Fog",
+    "260": "Fog",
+    "263": "LightShowers",
+    "266": "LightRain",
+    "281": "LightSleet",
+    "284": "LightSleet",
+    "293": "LightRain",
+    "296": "LightRain",
+    "299": "HeavyShowers",
+    "302": "HeavyRain",
+    "305": "HeavyShowers",
+    "308": "HeavyRain",
+    "311": "LightSleet",
+    "314": "LightSleet",
+    "317": "LightSleet",
+    "320": "LightSnow",
+    "323": "LightSnowShowers",
+    "326": "LightSnowShowers",
+    "329": "HeavySnow",
+    "332": "HeavySnow",
+    "335": "HeavySnowShowers",
+    "338": "HeavySnow",
+    "350": "LightSleet",
+    "353": "LightShowers",
+    "356": "HeavyShowers",
+    "359": "HeavyRain",
+    "362": "LightSleetShowers",
+    "365": "LightSleetShowers",
+    "368": "LightSnowShowers",
+    "371": "HeavySnowShowers",
+    "374": "LightSleetShowers",
+    "377": "LightSleet",
+    "386": "ThunderyShowers",
+    "389": "ThunderyHeavyRain",
+    "392": "ThunderySnowShowers",
+    "395": "HeavySnowShowers",
+}
+
+WEATHER_SYMBOL = {
+    "Unknown": "✨",
+    "Cloudy": "☁️",
+    "Fog": "🌫",
+    "HeavyRain": "🌧",
+    "HeavyShowers": "🌧",
+    "HeavySnow": "❄️",
+    "HeavySnowShowers": "❄️",
+    "LightRain": "🌦",
+    "LightShowers": "🌦",
+    "LightSleet": "🌧",
+    "LightSleetShowers": "🌧",
+    "LightSnow": "🌨",
+    "LightSnowShowers": "🌨",
+    "PartlyCloudy": "⛅️",
+    "Sunny": "☀️",
+    "ThunderyHeavyRain": "🌩",
+    "ThunderyShowers": "⛈",
+    "ThunderySnowShowers": "⛈",
+    "VeryCloudy": "☁️",
+}
 
 
 if __name__ == "__main__":
